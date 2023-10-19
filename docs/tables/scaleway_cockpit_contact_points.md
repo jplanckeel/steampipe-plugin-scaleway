a6e431aa02aa03d5fd7ca70d2f3b6985a06d2804
jplanckeel <jplanckeel@scaleway.com>
2023-10-19 15:31:57 +0200

    fix: tabvle cockpit contact endpoint
    Signed-off-by: jplanckeel <jplanckeel@scaleway.com>

---
+++ docs/tables/scaleway_cockpit_contact_points.md
@@ -0,0 +1,33 @@
+# Table: scaleway_iam_api_key
+
+Contact points define who is notified when an alert fires. Contact points include emails, Slack, on-call systems and texts. When an alert fires, all contact points are notified.
+
+This table requires an Organization ID to be configured in the scaleway.spc file. Because we use table scaleway_project in ParentHydrate. 
+
+## Examples
+
+### Basic info
+
+```sql
+select
+  email,
+  project
+from
+  scaleway_cockpit_contact_points;
+```
+
+### List contact for a project
+
+```sql
+select
+   email,
+   project as project_id,
+   p.name as project_name 
+from
+   scaleway_cockpit_contact_points as c 
+   inner join
+      scaleway_project p 
+      on c.project = p.project_id 
+where
+   p.name = 'default';
+```
--- scaleway/table_scaleway_cockpit_contact_points.go
+++ scaleway/table_scaleway_cockpit_contact_points.go
@@ -3,46 +3,30 @@ package scaleway
 import (
 	"context"
 
-	"github.com/scaleway/scaleway-sdk-go/api/cockpit/v1beta1"
+	"github.com/scaleway/scaleway-sdk-go/api/account/v2"
+	cockpit "github.com/scaleway/scaleway-sdk-go/api/cockpit/v1beta1"
 
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
 
 //// TABLE DEFINITION
 
 func tableScalewayCockpitContactPoints(_ context.Context) *plugin.Table {
 	return &plugin.Table{
-		Name:              "scaleway_cockpit_contact_points",
-		Description:       "A Compute Instance bare metal is a physical server in Scaleway.",
-		GetMatrixItemFunc: BuildZoneList,
+		Name:        "scaleway_cockpit_contact_points",
+		Description: "Contact points define who is notified when an alert fires in Scaleway.",
 		List: &plugin.ListConfig{
-			Hydrate:    listCockpitContactPoints,
-			KeyColumns: []*plugin.KeyColumn{},
+			ParentHydrate: listProjects,
+			Hydrate:       listCockpitContactPoints,
+			KeyColumns:    []*plugin.KeyColumn{},
 		},
 		Columns: []*plugin.Column{
 			{
-				Name:        "totalcount",
-				Description: "Count of all contact points created.",
+				Name:        "email",
+				Description: "Contact points created.",
 				Type:        proto.ColumnType_STRING,
-			},
-			{
-				Name:        "contact_points",
-				Description: "Array of contact points",
-				Type:        proto.ColumnType_JSON,
-				Transform:   transform.FromField("ContactPoints"),
-			},
-			{
-				Name:        "has_additional_receivers",
-				Description: "Specifies whether the contact point has other receivers than the default receiver.",
-				Type:        proto.ColumnType_BOOL,
-			},
-			{
-				Name:        "has_additional_contact_points",
-				Description: "Specifies whether there are unmanaged contact points.",
-				Type:        proto.ColumnType_BOOL,
 			},
 
 			// Scaleway standard columns
@@ -51,18 +35,18 @@ func tableScalewayCockpitContactPoints(_ context.Context) *plugin.Table {
 				Description: "The ID of the project where the server resides.",
 				Type:        proto.ColumnType_STRING,
 			},
-			{
-				Name:        "organization",
-				Description: "The ID of the organization where the server resides.",
-				Type:        proto.ColumnType_STRING,
-			},
 		},
 	}
 }
 
+type cockpitContactInfo = struct {
+	Email   string
+	Project string
+}
+
 //// LIST FUNCTION
 
-func listCockpitContactPoints(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+func listCockpitContactPoints(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 
 	// Create client
 	client, err := getSessionConfig(ctx, d)
@@ -71,12 +55,15 @@ func listCockpitContactPoints(ctx context.Context, d *plugin.QueryData, _ *plugi
 		return nil, err
 	}
 
-	// Create SDK objects for Scaleway Baremetal product
+	// Create SDK objects for Scaleway Cockpit product
 	cockpitApi := cockpit.NewAPI(client)
+
+	// Get Project details
+	projectData := h.Item.(*account.Project)
 
 	req := &cockpit.ListContactPointsRequest{
 		Page:      scw.Int32Ptr(1),
-		ProjectID: "",
+		ProjectID: projectData.ID,
 	}
 
 	// Retrieve the list of contact points
@@ -92,16 +79,16 @@ func listCockpitContactPoints(ctx context.Context, d *plugin.QueryData, _ *plugi
 	req.PageSize = scw.Uint32Ptr(uint32(maxResult))
 
 	var count int
-
 	for {
 		resp, err := cockpitApi.ListContactPoints(req)
 		if err != nil {
 			plugin.Logger(ctx).Error("scaleway_cockpit_contact_points.listCockpitContactPoints", "query_error", err)
-			return nil, err
+			//Break if cockpit does not exist in project
+			break
 		}
 
-		for _, baremetal := range resp.ContactPoints {
-			d.StreamListItem(ctx, baremetal)
+		for _, contactPoint := range resp.ContactPoints {
+			d.StreamListItem(ctx, cockpitContactInfo{contactPoint.Email.To, projectData.ID})
 
 			// Increase the resource count by 1
 			count++
@@ -111,7 +98,6 @@ func listCockpitContactPoints(ctx context.Context, d *plugin.QueryData, _ *plugi
 				return nil, nil
 			}
 		}
-
 		if resp.TotalCount == uint32(count) {
 			break
 		}
